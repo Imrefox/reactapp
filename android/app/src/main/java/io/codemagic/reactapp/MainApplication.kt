05ce9d0e31b40fe1a6f98a83b74760985ba25a96
Imrefox <karl.suhajda@yahoo.com>
2026-03-16 16:21:56 +0100

    Android codepush setup

--- android/app/src/main/java/io/codemagic/reactapp/MainApplication.kt
+++ android/app/src/main/java/io/codemagic/reactapp/MainApplication.kt
@@ -7,21 +7,26 @@ import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 
+
+// 1. Import the plugin class.
+import com.microsoft.codepush.react.CodePush
+
 class MainApplication : Application(), ReactApplication {
 
-  override val reactHost: ReactHost by lazy {
-    getDefaultReactHost(
-      context = applicationContext,
-      packageList =
-        PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
-        },
-    )
-  }
+    override val reactHost: ReactHost by lazy {
+        getDefaultReactHost(
+            context = applicationContext,
+            packageList = PackageList(this).packages.apply {
+                // Packages that cannot be autolinked yet can be added manually here
+                // add(MyReactNativePackage())
+            },
+            // 2. Set jsBundleFilePath so CodePush resolves the JS bundle at startup
+            jsBundleFilePath = CodePush.getJSBundleFile(),
+        )
+    }
 
-  override fun onCreate() {
-    super.onCreate()
-    loadReactNative(this)
-  }
+    override fun onCreate() {
+        super.onCreate()
+        loadReactNative(this)
+    }
 }
